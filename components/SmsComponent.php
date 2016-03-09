4bc632a327f87ed02cf07254bf24c272333b0d90
Sergey Okatov <sokatov@gmail.com>
2016-03-09 11:03:48 +0000

    Garbage is removed

--- components/SmsComponent.php
+++ components/SmsComponent.php
@@ -5,65 +5,12 @@ use Yii;
 use yii\base\Component;
 use yii\base\InvalidConfigException;
 
- /*
-
- * Параметры
- * msg - текст сообщения
- * clientId - кому отослать
- * action - действие, о котором информируем абонента (разбирается программой)
-  /
-
-function PushMessage(&$params, &$res){
-// параметры
- $msg = $params["msg"];
- $clientId = $params['clientId'];
- $action = $params['action'];
-// константы
- $url='https://gcm-http.googleapis.com/gcm/send';
- $headers=array('Content-Type: application/json; charset=UTF-8',
-  'Authorization: key=AIzaSyAgqJk5WLkboD7rprEwl8_4aEhr4XWTmDQ');
- $q="select push_token from sessions where status=1 and project_id=2 and user_id=$clientId order by start_time desc limit 1";
- $row=querySingle($q,$res);
- $data=array('message'=>$msg, 'action'=>$action);
- $notification=array('text'=>$msg, 'body'=>$msg,'title'=>'INET-TAXI',icon=>"myicon");
- if (isset($row['push_token'])) {
-  $deviceToken = $row["push_token"];
-  $postdata=array(
-   "data"=>$data,
-   "notification"=>$notification,
-   "to"=>$deviceToken);
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postdata));
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  $r = curl_exec($ch);
-  curl_close($ch);
-
-  //$r=http_post_fields($url,$data,null,array("headers"=>$headers));
-  if (!$r){
-   $res["error"]["code"]=ERR_BAD_PARAMETERS;
-   $res["error"]["msg"]='Push сервер вернул ошибку ';
-  }
-  else {
-   echo $r;
-   $res["error"]["code"]=0;
-   $res["error"]["msg"]='';
-  }
- }
- else {
-  $res["error"]["code"]=ERR_BAD_PARAMETERS;
-  $res["error"]["msg"]=$q;//'Не удалось найти push token для '.$clientId;
- }
-}
-*/
-
 class SmsComponent extends Component
 {
     public $url;
     public $account;
     public $password;
+    public $errors;
     public $sender;
 
     private $_text;
